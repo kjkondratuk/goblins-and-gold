e4593fac28fb70fefdcaaf7bca5f11b06780176f
Kevin Kondratuk <paintface07@gmail.com>
2022-04-07 23:00:37 -0500

    add run configuration and a way to attach debug commands when testing

--- main.go
+++ main.go
@@ -29,6 +29,8 @@ func main() {
 	exit := make(chan os.Signal, 1)
 	signal.Notify(exit, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
 
+	isDebug := strings.EqualFold(os.Getenv("DEBUG"), "true")
+
 	fmt.Println("Loading world...")
 	w := config.Read[world.World]("./data/test_world.yaml")
 
@@ -41,46 +43,62 @@ func main() {
 	}
 
 	fmt.Println("Configuring application...")
+	commands := []cli.Command{
+		{
+			Name:        "look",
+			Aliases:     []string{"l"},
+			Description: "Look at your surroundings",
+			Action: func(c *cli.Context) error {
+				//fmt.Println("Looking around...")
+				fmt.Println(s.CurrRoom.Description)
+				return nil
+			},
+		},
+		{
+			Name:        "stat",
+			Aliases:     []string{"s"},
+			Description: "Interrogate your player stats",
+			Action: func(c *cli.Context) error {
+				ps, _ := yaml.Marshal(s.Player)
+				fmt.Println(pterm.Green(string(ps)))
+				return nil
+			},
+		},
+		{
+			Name:        "quit",
+			Aliases:     []string{"q"},
+			Description: "Quit the game",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Quitting...")
+				exit <- syscall.SIGTERM
+				return nil
+			},
+		},
+	}
+
+	// if we're debugging add some additional debug commands that spoil the magic
+	if isDebug {
+		commands = append(commands, cli.Command{
+			Name:        "world",
+			Aliases:     []string{"w"},
+			Description: "Print general info about the world.",
+			Action: func(c *cli.Context) error {
+				ws, _ := yaml.Marshal(w)
+				fmt.Println(pterm.Green(string(ws)))
+				return nil
+			},
+		})
+	}
+
 	app := &cli.App{
 		Name: defaultApp,
 		Action: func(c *cli.Context) error {
 			fmt.Println("Running app...")
-
 			return nil
 		},
-		Commands: []cli.Command{
-			{
-				Name:        "look",
-				Aliases:     []string{"l"},
-				Description: "Look at your surroundings",
-				Action: func(c *cli.Context) error {
-					//fmt.Println("Looking around...")
-					fmt.Println(s.CurrRoom.Description)
-					return nil
-				},
-			},
-			{
-				Name:        "stat",
-				Aliases:     []string{"s"},
-				Description: "Interrogate your player stats",
-				Action: func(c *cli.Context) error {
-					ps, _ := yaml.Marshal(s.Player)
-					fmt.Println(pterm.Green(string(ps)))
-					return nil
-				},
-			},
-			{
-				Name:        "quit",
-				Aliases:     []string{"q"},
-				Description: "Quit the game",
-				Action: func(c *cli.Context) error {
-					fmt.Println("Quitting...")
-					exit <- syscall.SIGTERM
-					return nil
-				},
-			},
-		},
+		Commands: commands,
 	}
+
 	ac := readline.NewPrefixCompleter()
 	rl, err := readline.NewEx(&readline.Config{
 		Prompt:       pterm.LightYellow(" >> "),
@@ -96,11 +114,14 @@ func main() {
 	// REPL Loop
 	go func() {
 		for {
+			// prompt the user and read the input
 			line, err := rl.Readline()
 			if err != nil {
 				fmt.Printf("Error: %+v\n", err)
 				exit <- syscall.SIGTERM
 			}
+
+			// tokenize the command arguments for processing
 			args := strings.Split(line, " ")
 
 			// prepend the "app" qualifier to all calls to execute the default application
@@ -110,10 +131,6 @@ func main() {
 			if err != nil {
 				fmt.Println(err)
 			}
-			//ps, _ := json.MarshalIndent(p, "", "  ")
-			//fmt.Print(pterm.Green(fmt.Sprintf("Player: %s\n", ps)))
-			//ws, _ := json.MarshalIndent(w, "", "  ")
-			//fmt.Print(pterm.Cyan(fmt.Sprintf("World: %s\n", ws)))
 		}
 	}()
 
--- world/world.go
+++ world/world.go
@@ -7,6 +7,12 @@ type World struct {
 	StartRoom string               `yaml:"startingRoom"`
 }
 
+// Room : retrieves a room by its key from the rooms present in this world
+// Parameters:
+//   - key - string - the key of the room
+// Return:
+//   - room.Room - the room
+//   - bool - whether or not the room could be located
 func (w World) Room(key string) (room.Room, bool) {
 	if r, ok := w.Rooms[key]; ok {
 		return r, true
