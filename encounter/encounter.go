ec97850268432283218a055675261cb159746b53
Kevin Kondratuk <k.kondratuk@fetchrewards.com>
2022-04-23 09:36:12 -0500

    add turn order looping

--- encounter/encounter.go
+++ encounter/encounter.go
@@ -1,6 +1,7 @@
 package encounter
 
 import (
+	"container/ring"
 	"fmt"
 	"github.com/kjkondratuk/goblins-and-gold/actors"
 	"github.com/kjkondratuk/goblins-and-gold/actors/monster"
@@ -47,37 +48,61 @@ func NewEncounter(d Definition) Encounter {
 	}
 }
 
+func calcIndexKey(c actors.Combatant, dupCounter int) string {
+	return fmt.Sprintf("%02d/%02d/%d", c.Roll(dice.D20)+c.BaseStats().DexMod(), c.BaseStats().Dex, dupCounter)
+}
+
 func (e *encounter) Run(p player.Player) Outcome {
 	pterm.Info.Println(fmt.Sprintf("Running encounter..."))
 
-	// Create a map of combatants and keep a list of keys for sequencing combat
-	// TODO : need to figure out a tie breaker when combatants roll the same values since we're using a map here
-	turns := make(map[int]actors.Combatant)
-	inits := make([]int, 1+len(e._enemies))
-	pInit := p.Roll(dice.D20) + p.BaseStats().DexMod()
-	turns[pInit] = p
-	inits[0] = pInit
-	for i, e := range e._enemies {
-		inits[i+1] = e.BaseStats().DexMod() + e.Roll(dice.D20)
-		turns[inits[i+1]] = e
+	// Create a map of combatants with an index representing their turn order
+	dupCounter := 0
+	fighters := make(map[string]actors.Combatant)
+	initiatives := make([]string, 1+len(e._enemies))
+
+	// Roll initiative for the player and add them to the combatant map at the given index
+	// index format is: <2 digit padded roll total>/<2 digit padded dexterity score>/<duplicate count>
+	initiatives[0] = calcIndexKey(p, dupCounter)
+	fighters[initiatives[0]] = p
+
+	for i, enemy := range e._enemies {
+		// calculate an index key -- increment and recalculate if there is a collision
+		newIndexKey := calcIndexKey(enemy, dupCounter)
+		if _, collides := fighters[newIndexKey]; collides {
+			dupCounter++
+			newIndexKey = calcIndexKey(enemy, dupCounter)
+		}
+
+		// add the combatant to the participants and add index value
+		initiatives[i+1] = newIndexKey
+		fighters[initiatives[i+1]] = enemy
 	}
 
 	// Sort in ascending order, then reverse it
-	sort.Ints(inits)
-	for i, j := 0, len(inits)-1; i < j; i, j = i+1, j-1 {
-		inits[i], inits[j] = inits[j], inits[i]
+	sort.Strings(initiatives)
+	for i, j := 0, len(initiatives)-1; i < j; i, j = i+1, j-1 {
+		initiatives[i], initiatives[j] = initiatives[j], initiatives[i]
+	}
+
+	// populate the turn order key values into a circular data structure we can iterate
+	turnOrder := ring.New(len(initiatives))
+	for _, x := range initiatives {
+		turnOrder.Value = x
+		turnOrder = turnOrder.Next()
 	}
 
 	// TODO : finish implementing encounters
 	// loop over list, taking a turn for each combatant
-	idx := 0
-	endCombat := false
-	for !endCombat {
-		// players will select an attack from available options, and a target(s) for the attack
-
-		endCombat = true
-		idx++
+	curr := turnOrder
+	for i := 0; i < len(initiatives); i++ {
+		pterm.Success.Printfln("%s -> %s", curr.Value, curr.Next().Value)
+		curr = curr.Move(1)
 	}
+	//turnOrder.Do(func(a any) {
+	//	key := a.(int)
+	//	pterm.Success.Printfln("Taking turn: %s", key)
+	//	turnOrder.
+	//})
 	// monsters will roll a die to select an attack from an attack distribution, or by random by default
 	// Calculate to-hit based on the combatant's modifier
 	// If the attack hits, calculate damage
